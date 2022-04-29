7fd00e871fdbca8e2350fa59dc59813aa0c3d5db
don <stivnnkolov@gmail.com>
2022-04-30 00:57:39 +0300

    Added mixin for accounts forms

--- cam_0504/cam_0504/accounts/forms.py
+++ cam_0504/cam_0504/accounts/forms.py
@@ -2,17 +2,14 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth import forms as auth_forms
 
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
 
 from cam_0504.accounts.models import Profile
+from common.mixins import CrispyFormHelperCustomMixin, CrispyFormHelperBuildinMixin
 
 UserModel = get_user_model()
 
 
-# TODO some mixin for the helper property. Make em all the same
-
-class UserRegisterForm(auth_forms.UserCreationForm):
+class UserRegisterForm(CrispyFormHelperCustomMixin, auth_forms.UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -20,56 +17,13 @@ class UserRegisterForm(auth_forms.UserCreationForm):
         model = UserModel
         fields = ['email', 'password1', 'password2']
 
-    @property
-    def helper(self):
-        helper = FormHelper()
-        helper.form_method = 'POST'
-        helper.layout = Layout(
 
-        )
-        for field in self.Meta().fields:
-            helper.layout.append(
-                Field(field, wrapper_class='row')
-            )
-        helper.layout.append(
-            Submit(
-                'submit',
-                'Submit',
-                css_class='btn btn-md btn-secondary',
-            )
-        )
-        helper.field_class = 'col-10 mb-3'
-        helper.label_class = 'col-2'
-        return helper
-
-
-class UserLogInForm(auth_forms.AuthenticationForm):
+class UserLogInForm(CrispyFormHelperBuildinMixin, auth_forms.AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @property
-    def helper(self):
-        helper = FormHelper()
-        helper.form_method = 'POST'
-        helper.layout = Layout(
 
-        )
-        for field in self.fields:
-            helper.layout.append(
-                Field(field, wrapper_class='row')
-            )
-        helper.layout.append(
-            Submit(
-                'submit',
-                'Log in',
-                css_class='btn btn-md btn-secondary',
-            ))
-        helper.field_class = 'col-10 mb-3'
-        helper.label_class = 'col-2'
-        return helper
-
-
-class ProfileEditForm(forms.ModelForm):
+class ProfileEditForm(CrispyFormHelperCustomMixin, forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['first_name', 'last_name', 'age', 'city_name', 'email', 'restaurant_name']
@@ -89,55 +43,10 @@ class ProfileEditForm(forms.ModelForm):
             ),
         }
 
-    @property
-    def helper(self):
-        helper = FormHelper()
-        helper.form_method = 'POST'
-        helper.layout = Layout(
 
-        )
-        for field in self.Meta().fields:
-            helper.layout.append(
-                Field(field, wrapper_class='row'),
-            )
-        helper.layout.append(Submit(
-            'submit',
-            'Submit',
-            css_class='brn btn-md btn-secondary',
-        ))
-        helper.field_class = 'col-9 mb-3'
-        helper.label_class = 'col-3'
-
-        return helper
-
-
-class ChangeUserPasswordForm(auth_forms.PasswordChangeForm):
+class ChangeUserPasswordForm(CrispyFormHelperBuildinMixin, auth_forms.PasswordChangeForm):
     def __init__(self, user, *args, **kwargs):
         super().__init__(user, *args, **kwargs)
-
-    @property
-    def helper(self):
-        helper = FormHelper()
-        helper.form_method = 'POST'
-        helper.layout = Layout(
-
-        )
-        for field in self.fields:
-            helper.layout.append(
-                Field(field, wrapper_class='row')
-            )
-        helper.layout.append(
-            Submit(
-                'submit',
-                'Submit',
-                css_class='btn btn-md btn-secondary',
-
-            )
-        )
-        helper.field_class = 'col-10 mb-3'
-        helper.label_class = 'col-2'
-
-        return helper
 
 
 class ProfileDeleteForm(forms.ModelForm):
@@ -146,52 +55,11 @@ class ProfileDeleteForm(forms.ModelForm):
         fields = []
 
 
-class CustomPasswordResetForm(auth_forms.PasswordResetForm):
+class CustomPasswordResetForm(CrispyFormHelperBuildinMixin, auth_forms.PasswordResetForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @property
-    def helper(self):
-        helper = FormHelper()
-        helper.form_method = 'POST'
-        helper.layout = Layout()
-        for field in self.fields:
-            helper.layout.append(
-                Field(field, wrapper_class='row')
-            )
-        helper.layout.append(
-            Submit(
-                'submit',
-                'Submit',
-                css_class='btn btn-md mt-2 btn-secondary',
-            )
-        )
 
-        return helper
-
-
-class CustomSetPasswordForm(auth_forms.SetPasswordForm):
+class CustomSetPasswordForm(CrispyFormHelperCustomMixin, auth_forms.SetPasswordForm):
     def __init__(self, user, *args, **kwargs):
         super().__init__(user, *args, **kwargs)
-
-    @property
-    def helper(self):
-        helper = FormHelper()
-        helper.form_method = 'POST'
-        helper.layout = Layout()
-
-        for field in self.fields:
-            helper.layout.append(
-                Field(field, wrapper_class='row')
-            )
-        helper.layout.append(
-            Submit(
-                'submit',
-                'Submit',
-                css_class='btn btn-md mt-3 btn-secondary',
-            )
-        )
-
-        helper.field_class = 'col-9 mb-3 mt-3'
-        helper.label_class = 'col-3 mt-3'
-        return helper
--- cam_0504/common/mixins.py
+++ cam_0504/common/mixins.py
@@ -34,3 +34,52 @@ class CrispyFormHelperMixin:
         helper.label_class = 'mb-3'
 
         return helper
+
+
+class CrispyFormHelperCustomMixin:
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.form_method = 'POST'
+        helper.layout = Layout(
+
+        )
+        for field in self.Meta().fields:
+            helper.layout.append(
+                Field(field, wrapper_class='row'),
+            )
+        helper.layout.append(Submit(
+            'submit',
+            'Submit',
+            css_class='btn btn-md btn-secondary',
+        ))
+        helper.field_class = 'col-9 mb-3'
+        helper.label_class = 'col-3'
+
+        return helper
+
+
+class CrispyFormHelperBuildinMixin:
+    @property
+    def helper(self):
+        helper = FormHelper()
+        helper.form_method = 'POST'
+        helper.layout = Layout(
+
+        )
+        for field in self.fields:
+            helper.layout.append(
+                Field(field, wrapper_class='row')
+            )
+        helper.layout.append(
+            Submit(
+                'submit',
+                'Submit',
+                css_class='btn btn-md btn-secondary',
+
+            )
+        )
+        helper.field_class = 'col-10 mb-3'
+        helper.label_class = 'col-2'
+
+        return helper
